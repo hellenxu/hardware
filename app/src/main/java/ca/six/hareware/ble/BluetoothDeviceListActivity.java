f0d2579b54e1863df08adf0bbd5c810be2cd1e77
hellenxu <heavensxu@gmail.com>
2017-06-26 23:19:59 -0400

    Add: recyclerview dependencies.

---
+++ app/src/main/java/ca/six/hareware/ble/BluetoothDeviceListActivity.java
@@ -0,0 +1,19 @@
+package ca.six.hareware.ble;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * @copyright six.ca
+ * Created by Xiaolin on 2017-06-26.
+ */
+
+public class BluetoothDeviceListActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
