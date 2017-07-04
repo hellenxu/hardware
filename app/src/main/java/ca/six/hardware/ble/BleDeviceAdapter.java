d9e7f7fc0e2b581842cfe197fb21ce03e5dfc947
XiaolinXu <xxu@kpd-i.com>
2017-07-04 16:16:31 -0400

    Add: scan bluetooth device.

---
+++ app/src/main/java/ca/six/hardware/ble/BleDeviceAdapter.java
@@ -0,0 +1,80 @@
+package ca.six.hardware.ble;
+
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ca.six.hareware.R;
+
+/**
+ * @author hellenxu
+ * @date 2017-07-04
+ * Copyright 2017 Six. All rights reserved.
+ */
+
+public class BleDeviceAdapter extends RecyclerView.Adapter<BleDeviceAdapter.BleDeviceHolder> {
+    private LayoutInflater inflater;
+    private List<BluetoothDevice> deviceList = new ArrayList<>();
+
+    public BleDeviceAdapter(Context ctx, List<BluetoothDevice> data){
+        if(null == ctx){
+            throw new IllegalArgumentException("Context is null");
+        }
+
+        inflater = LayoutInflater.from(ctx);
+        if(null != data){
+            deviceList = data;
+        }
+    }
+
+    public void setData(List<BluetoothDevice> data){
+        if(null != data) {
+            deviceList = data;
+        }
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public BleDeviceHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = inflater.inflate(R.layout.item_device_list, parent, false);
+        return new BleDeviceHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(BleDeviceHolder holder, int position) {
+        final BluetoothDevice device = deviceList.get(position);
+        final String deviceName = TextUtils.isEmpty(device.getName()) ? "Unknown" : device.getName();
+        final String deviceAddress = TextUtils.isEmpty(device.getAddress()) ? "Unknown" : device.getAddress();
+        holder.tvDeviceName.setText(deviceName);
+        holder.tvDeviceMac.setText(deviceAddress);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public int getItemCount() {
+        return deviceList.size();
+    }
+
+    class BleDeviceHolder extends RecyclerView.ViewHolder{
+        TextView tvDeviceName, tvDeviceMac;
+
+        BleDeviceHolder(View itemView) {
+            super(itemView);
+            tvDeviceName = (TextView) itemView.findViewById(R.id.tv_device_name);
+            tvDeviceMac = (TextView) itemView.findViewById(R.id.tv_device_mac);
+        }
+    }
+}
--- app/src/main/java/ca/six/hardware/ble/BluetoothDeviceListActivity.java
+++ app/src/main/java/ca/six/hardware/ble/BluetoothDeviceListActivity.java
@@ -1,8 +1,21 @@
 package ca.six.hardware.ble;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ca.six.hareware.R;
 
 /**
  * @copyright six.ca
@@ -10,10 +23,82 @@ import android.support.annotation.Nullable;
  */
 
 public class BluetoothDeviceListActivity extends Activity {
+    private BluetoothAdapter mBleAdapter;
+    private List<BluetoothDevice> bluetoothDeviceList = new ArrayList<>();
+    private Handler mHandler = new Handler();
+    private boolean isScanning;
+    private TextView tvStatus;
+    private BleDeviceAdapter mDeviceAdapter;
+
+    private static final int BLUETOOTH_ENABLE_REQUEST = 100;
+    private static final int SCAN_PERIOD = 2000;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_ble_list);
 
+        mBleAdapter = BluetoothAdapter.getDefaultAdapter();
+        tvStatus = (TextView) findViewById(R.id.tv_status);
+        tvStatus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scanLeDevice(isBluetoothEnabled() && !isScanning);
+            }
+        });
+
+        mDeviceAdapter = new BleDeviceAdapter(this, bluetoothDeviceList);
+        RecyclerView rvDeviceList = (RecyclerView) findViewById(R.id.rv_device_list);
+        rvDeviceList.setLayoutManager(new LinearLayoutManager(this));
+        rvDeviceList.setAdapter(mDeviceAdapter);
     }
+
+    //check whether bluetooth is enabled.
+    public boolean isBluetoothEnabled(){
+        return null != mBleAdapter && mBleAdapter.isEnabled() && !mBleAdapter.isDiscovering();
+    }
+
+    public void requestBluetoothEnable(){
+        if(null != mBleAdapter && !mBleAdapter.isEnabled()){
+            Intent enabledBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enabledBluetooth, BLUETOOTH_ENABLE_REQUEST);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == Activity.RESULT_OK && requestCode == BLUETOOTH_ENABLE_REQUEST){
+            //do things after bluetooth enabled
+        }
+    }
+
+    private void scanLeDevice(final boolean enable){
+        if(enable){
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    isScanning = false;
+                    mBleAdapter.stopLeScan(leScanCallback);
+                    mDeviceAdapter.setData(bluetoothDeviceList);
+                    tvStatus.setText(R.string.start_scan);
+                }
+            }, SCAN_PERIOD);
+            isScanning = true;
+            mBleAdapter.startLeScan(leScanCallback);
+            tvStatus.setText(R.string.stop_scan);
+        } else {
+            isScanning = false;
+            mBleAdapter.stopLeScan(leScanCallback);
+        }
+    }
+
+    //rssi: received signal strength indicator
+    private BluetoothAdapter.LeScanCallback leScanCallback = new BluetoothAdapter.LeScanCallback() {
+        @Override
+        public void onLeScan(BluetoothDevice device, int rssi, byte[] scanRecord) {
+            bluetoothDeviceList.add(device);
+            System.out.println("xxl - rssi: " + rssi);
+        }
+    };
 }
